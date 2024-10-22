8d1b7ce2ce50ecd42f7c36bcf62b235bba599738
unknown <75647984+ha308ing@users.noreply.github.com>
2024-10-22 14:21:42 +0400

    config: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,24 @@
+import { resolve } from "node:path";
+
+const __dirname = import.meta.dirname;
+
 /** @type {import("webpack").Configuration} */
-export default {};
+export default {
+    entry: resolve(__dirname, "src/index.ts"),
+    output: {
+        clean: true,
+        filename: "index.cjs",
+        path: resolve(__dirname, "dist"),
+    },
+    target: ["es6", "node22.9"],
+    mode: "production",
+    resolve: {
+        extensions: [".ts"],
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+    },
+    module: {
+        rules: [{ test: /\.ts$/, loader: "ts-loader" }],
+    },
+};
